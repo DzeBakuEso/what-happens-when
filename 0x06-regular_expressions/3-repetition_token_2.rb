971e2153571a2d678a03add769549ec28e7f1f6e
DzeBakuEso <dzeblebaku@gmail.com>
2024-11-05 17:16:52 -0800

    3-repetition_token_2.rb

---
+++ 0x06-regular_expressions/3-repetition_token_2.rb
@@ -0,0 +1,15 @@
+#!/usr/bin/env ruby
+
+# Check if an argument is provided
+if ARGV.empty?
+  puts ""
+  exit
+end
+
+# Match the pattern with the provided argument
+matches = ARGV[0].scan(/^hbt+n$/)
+
+# Display the input and the matches
+puts "Input received: #{ARGV[0]}"
+puts "Matches: #{matches.empty? ? 'No matches found.' : matches}"
+puts matches.join
