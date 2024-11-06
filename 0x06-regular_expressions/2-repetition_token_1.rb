b266a7db4f504d21af0ccf3e63c8d62fb51c0fbb
DzeBakuEso <dzeblebaku@gmail.com>
2024-11-05 18:40:06 -0800

    2-repetition_token_1.rb

--- 0x06-regular_expressions/2-repetition_token_1.rb
+++ 0x06-regular_expressions/2-repetition_token_1.rb
@@ -6,10 +6,6 @@ if ARGV.empty?
   exit
 end
 
-# Match the pattern with the provided argument
-matches = ARGV[0].scan(/^hb?tn$/)
-
-# Display the input and the matches
-puts "Input received: #{ARGV[0]}"
-puts "Matches: #{matches.empty? ? 'No matches found.' : matches}"
-puts matches.join
+# Match "htn" or "hbtn"
+match = ARGV[0].scan(/^hb?tn$/)
+puts match.join
