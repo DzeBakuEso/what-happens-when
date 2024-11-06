9284c764d14969adf9ea34762f313da67a68e87e
DzeBakuEso <dzeblebaku@gmail.com>
2024-11-05 17:08:36 -0800

    2-repetition_token_1.rb

--- 0x06-regular_expressions/2-repetition_token_1.rb
+++ 0x06-regular_expressions/2-repetition_token_1.rb
@@ -1,15 +1,15 @@
 #!/usr/bin/env ruby
 
-# Check to see if an argmnt is passed
+# Check if an argument is provided
 if ARGV.empty?
-  puts "argument not passed"
+  puts ""
   exit
 end
 
-# Matching the pattern with the argument passed
-matches = ARGV[0].scan(/^[Hh]b+t+n$/)
+# Match the pattern with the provided argument
+matches = ARGV[0].scan(/^hb?tn$/)
 
-# Display both input and the matches
+# Display the input and the matches
 puts "Input received: #{ARGV[0]}"
 puts "Matches: #{matches.empty? ? 'No matches found.' : matches}"
 puts matches.join
